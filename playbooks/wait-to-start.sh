cd6bff6be387307f0d12141470eb754517ce23b5
Dzintars Klavins <dzintars.klavins@gmail.com>
2023-06-02 14:46:10 +0300

    Initial Podman version
    At least it runs all the components.
    There still are some communication issues.
    Zuul does not see nodes and seems it cant communicate with Gerrit.

--- playbooks/wait-to-start.sh
+++ playbooks/wait-to-start.sh
@@ -17,7 +17,7 @@ wait_for_mysql() {
 wait_for_gerrit() {
     echo `date -Iseconds` "Wait for zuul user to be created"
     for i in $(seq 1 120); do
-        [ $(curl -s -o /dev/null -w "%{http_code}" http://admin:secret@gerrit:8080/a/accounts/zuul/sshkeys) = "200" ] && return
+        [ $(curl -s -o /dev/null -w "%{http_code}" http://admin:secret@gerrit:9280/a/accounts/zuul/sshkeys) = "200" ] && return
         sleep 1
     done
 
